5f9156d63a4f50fe61d704edb30c4d0a962a57af
Alexander Pugachev <apugachev@griddynamics.com>
2012-09-20 17:58:06 +0300

    upload image

--- static/focus/js/main.js
+++ static/focus/js/main.js
@@ -3,3 +3,22 @@ $(function() {
     $(".chzn-select").chosen({allow_single_deselect: true});
     $(".help-tooltip").tooltip();
 });
+
+function UploadImageController($scope, $log){
+    $scope.$log = $log;
+    $scope.type = 'solid'//'amazon_like';
+
+    $scope.set_solid = function(e){
+        $scope.solid_url = e.currentTarget.title;
+        $scope.solid_name = e.currentTarget.text;
+    }
+
+    $scope.set_amazon = function(e){
+        $scope.amazon_name = e.currentTarget.text;
+        $scope.amazon_kernel_url = e.currentTarget.attributes['data-kernel'].textContent;
+        $scope.kernel_selection = '-1';
+        $scope.amazon_initrd_url = e.currentTarget.attributes['data-initrd'].textContent;
+        $scope.initrd_selection = '-1';
+        $scope.amazon_rootfs_url = e.currentTarget.attributes['data-rootfs'].textContent;
+    }
+}
