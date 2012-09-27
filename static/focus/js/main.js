70dd48e31f829e686bb0f3ce350794cccb4a9b3d
Alexander Pugachev <apugachev@griddynamics.com>
2012-09-27 10:25:17 +0300

    .

--- static/focus/js/main.js
+++ static/focus/js/main.js
@@ -1,6 +1,7 @@
 $(function() {
     $(".chzn-select").chosen({allow_single_deselect: true});
     $(".help-tooltip").tooltip();
+    $("[rel=tooltip]").tooltip();
     $(".daterange").daterangepicker();
 });
 
