d5aa0b48141ce0d1f5762a095a6a6000db93efac
Stanislav Pugachev <spugachev@griddynamics.com>
2012-09-27 13:41:22 +0300

    .

--- static/focus/js/main.js
+++ static/focus/js/main.js
@@ -2,7 +2,16 @@ $(function() {
     $(".chzn-select").chosen({allow_single_deselect: true});
     $(".help-tooltip").tooltip();
     $("[rel=tooltip]").tooltip();
-    $(".daterange").daterangepicker();
+    $(".daterange").daterangepicker({
+        ranges: {
+            'Today': ['today', 'today'],
+            'Yesterday': ['yesterday', 'yesterday'],
+            'Last 7 Days': [Date.today().add({ days: -6 }), 'today'],
+            'Last 30 Days': [Date.today().add({ days: -29 }), 'today'],
+            'This Month': [Date.today().moveToFirstDayOfMonth(), Date.today().moveToLastDayOfMonth()],
+            'Last Month': [Date.today().moveToFirstDayOfMonth().add({ months: -1 }), Date.today().moveToFirstDayOfMonth().add({ days: -1 })]
+        }
+    });
 });
 
 function Validator(){
