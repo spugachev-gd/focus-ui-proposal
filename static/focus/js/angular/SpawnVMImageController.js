3a409c6b733bb533a88e25b34c2cb4aa3bc8bed7
Alexander Pugachev <apugachev@griddynamics.com>
2012-10-08 12:14:37 +0300

    Add Image Form Behaviour

---
+++ static/focus/js/angular/SpawnVMImageController.js
@@ -0,0 +1,31 @@
+
+/**
+ * Created with PyCharm.
+ * User: peroksid
+ * Date: 10/5/12
+ * Time: 4:04 PM
+ * To change this template use File | Settings | File Templates.
+ */
+function SpawnVMImageController($scope){
+    $scope.images = [
+        {
+            v: ''
+        },
+        {
+            'v': '50e2761f-b93e-4277-af4e-17afef999296',
+            't': 'mini image'
+        }
+    ]
+    if (window.localStorage['additional_image_name']){
+        $scope.images.push(
+            {
+                'v': window.localStorage['additional_image_name'],
+                't': window.localStorage['additional_image_name']
+            })
+        $scope.image_selected =  window.localStorage['additional_image_name']
+    }
+
+}
+$(window).unload(function(){
+    window.localStorage.clear()
+})
